0e679a62635e01dfe45e3edb56b8979342133c69
Ruslan-Nikolaevich <ruslan_nic@mail.ru>
2021-08-21 20:48:36 +0300

    typical-arrays-problems

--- src/index.js
+++ src/index.js
@@ -1,12 +1,40 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (array===undefined||array.length<1) {
+    return 0;
+  }
+  let min=array[0];
+  for (let index = 0; index < array.length; index++) {
+    if(min>array[index]) {min=array[index]}
+    
+  }
+  return min;
 }
 
 exports.max = function max (array) {
-  return 0;
+  if (array===undefined||array.length<1) {
+    return 0;
+  }
+  let max=array[0];
+  for (let index = 0; index < array.length; index++) {
+    if(max<array[index]) {max=array[index]}
+    
+  }
+  return max;
+  
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (array===undefined||array.length<1) {
+    return 0;
+  }
+  let sum=0;
+  let avg;
+  for (let index = 0; index < array.length; index++) {
+    sum+=array[index];
+    
+  }
+  avg= sum/(array.length)
+  return avg;
+ 
 }
